a42345599a18bace8640f54e1613a87fcddd3a27
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-06-01 18:18:30 -0400

    Added links.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,7 @@ import itch from '../../Img/itch.png';
 import teepublic from '../../Img/teepublic.png';
 import tgc from '../../Img/tgc.png';
 import logo from '../../Img/01.png';
+import logo3 from '../../Img/03.png';
 
 class Main extends Component {
     state = {
@@ -28,23 +29,27 @@ class Main extends Component {
                         className="logo"
                         fluid
                     />
+                    <p className='subTitle'>Ponder Mint Company, Brand, and Project Breakdown</p>
+                    
                     <h5>Ponder Mint LLC (Parent Company)</h5>
-                    <p>                    <Image className='logo2' src={logo} />
-- Subsidiary: Ponder Enterprises LLC | Ponder Code | A.I. Chasm Games (A division of Ponder Enterprises).</p>
+                    
+                    <p><a href="https://www.pondermint.com/" target="_blank"><Image className='logo2' src={logo} /></a>- Subsidiary: Ponder Enterprises LLC | Ponder Code | A.I. Chasm Games (A division of Ponder Enterprises).</p>
+
                     <h5>Ponder Enterprises LLC | Ponder Code (Registered Alternate Name)</h5>
-                    <p>                    <Image className='logo2' src={etsy} />
-- Etsy Store: Fidget Ki (3D printed fidget gadget).</p>
-                    <p>                    <Image className='logo2' src={teepublic} />
-- TeePublic Store: custom nerdy shirts, stickers, mugs, and prints (largely programming related).</p>
-                    <p>                    <Image className='logo2' src={logo} />
-- Deployed Apps: Code Seonbi (Coding references and bug Tracker), Sholodex.com, the Show Lowdown Index (Streaming platform price guide and calculator).</p>
+
+                    <p><a href="https://www.etsy.com/shop/PonderEnterprises?ref=seller-platform-mcnav" target="_blank"><Image className='logo2' src={etsy} /></a>- Etsy Store: Fidget Ki (3D printed fidget gadget).</p>
+
+                    <p><a href="https://www.teepublic.com/user/ponder-print" target="_blank"><Image className='logo2' src={teepublic} /></a>- TeePublic Store: custom nerdy shirts, stickers, mugs, and prints (largely programming related).</p>
+                    
+                    <p><a href="https://www.pondercode.com/" target="_blank"><Image className='logo2' src={logo3} /></a>- Deployed Apps: Code Seonbi (Coding references and bug Tracker) | Sholodex.com, the Show Lowdown Index (Streaming platform price guide and calculator).</p>
+
                     <h5>A.I. Chasm Games, (Ponder Enterprises Tabletop and Video Games Branch)</h5>
-                    <p>                    <Image className='logo2' src={itch} />
-- Itch.io: Bug Buster (2D platformer with coding commentary), Schrodinger the Cat (2D top-down puzzle game).</p>
-                    <p>                    <Image className='logo2' src={google} />
-- Google Play: Flappy Flea (2D endless runner style game).</p>
-                    <p>                    <Image className='logo2' src={tgc} />
-- The Game Crafter: Codeck (Programming card game), Nerd Word (Competative nerd trivia card game), Routes and Reptiles (Race to the finish RPG board game).</p>
+
+                    <p><a href="https://a-i-chasm-games.itch.io/" target="_blank"><Image className='logo2' src={itch} /></a>- Itch.io: Bug Buster (2D platformer with coding commentary), Schrodinger the Cat (2D top-down puzzle game).</p>
+
+                    <p><a href="https://play.google.com/store/apps/developer?id=ak89mkii" target="_blank"><Image className='logo2' src={google} /></a>- Google Play: Flappy Flea (2D endless runner style game).</p>
+
+                    <p><a href="https://www.thegamecrafter.com/designers/ponder-mint-games" target="_blank"><Image className='logo2' src={tgc} /></a>- The Game Crafter: Codeck (Programming card game), Nerd Word (Competative nerd trivia card game), Routes and Reptiles (Race to the finish RPG board game).</p>
                 </Container>
             </div>
         )
