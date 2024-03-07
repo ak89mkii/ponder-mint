a3aff62cee6aa74a2be484e182f6998ae32721dd
ak89mkii <ak89mkii@gmail.com>
2024-03-07 13:42:18 -0500

    Fixed typo and footer date.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,7 @@ class Footer extends Component {
                 <div class="container">
                     <div class='row'>
                         <div class="col">
-                            © 2017 - 2023 Ponder Mint LLC
+                            © 2017 - 2024 Ponder Mint LLC
                         </div>
                     </div>
                 </div>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -14,7 +14,7 @@ class Main extends Component {
                 <Container>
                     <h1 className='title'>Ponder Mint</h1>
                     <p className='subTitle'>Invent, Innovate, Iterate</p>
-                    <p>Ponder Mint LLC is a company focused on research, design, and development ventures. As a parent comany, we support projects focused on inventive, innovative, and iterative initiatives, taking on and testing ideas.</p>
+                    <p>Ponder Mint LLC is a company focused on research, design, and development ventures. As a parent company, we support projects focused on inventive, innovative, and iterative initiatives, taking on and testing ideas.</p>
                     {/* <p className='subTitle'>Ponder Mint Company, Brand, and Project Breakdown</p> */}
                     <Image
                         alt=""
